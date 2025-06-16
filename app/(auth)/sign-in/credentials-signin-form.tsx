11c2f6946c68f30191f38df69bad697ba71497b9
Xuwei <qinxuwei15@gmail.com>
2025-06-16 13:27:45 +0200

    sign-in cg

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -5,10 +5,30 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { signInDefaultValues } from '@/lib/constants';
 import Link from 'next/link';
+import { signInWithCredentials } from '@/lib/actions/user.actions';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
+import { useSearchParams } from 'next/navigation';
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: '',
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" variant="default" disabled={pending}>
+        {pending ? 'Signing In...' : 'Sign In'}
+      </Button>
+    );
+  };
   return (
-    <form>
+    <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
       <div className="space-y-6">
         {' '}
         {/* 只保留一个 space-y-6 容器 */}
@@ -35,10 +55,11 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign In
-          </Button>
+          <SignInButton />
         </div>
+        {data && !data.success && (
+          <div className="text-destructive text-center">{data.message}</div>
+        )}
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{' '}
           <Link href="/sign-up" target="_self" className="link">
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,12 +10,24 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import CredentialsSignInForm from './credentials-signin-form';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Sign In',
 };
 
-const SignInPage = () => {
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+  const session = await auth();
+  if (session) {
+    return redirect(callbackUrl || '/');
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       {' '}
