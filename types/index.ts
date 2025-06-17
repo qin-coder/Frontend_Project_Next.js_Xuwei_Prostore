330f3a19fb30df5604396c2d158a74e463c38ff0
Xuwei <qinxuwei15@gmail.com>
2025-06-17 11:59:50 +0200

    order page

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,8 @@ import {
   insertProductSchema,
   insertCartSchema,
   cartItemSchema,
+  insertOrderSchema,
+  insertOrderItemSchema,
 } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -15,3 +17,15 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: { name: string; email: string };
+  //paymentResult: PaymentResult;
+};
