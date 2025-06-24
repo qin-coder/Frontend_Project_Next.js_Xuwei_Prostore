cbc14e2fef67663bb5f2a9f42be29a465fa70af2
Xuwei <qinxuwei15@gmail.com>
2025-06-24 22:55:52 +0200

    copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,12 @@
-import { APP_COPYRIGHT } from '@/lib/constants';
+import { APP_NAME } from '@/lib/constants';
 
 const Footer = () => {
-  //const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t">
-      <div className="p-5 flex-center">{APP_COPYRIGHT}</div>
+      <div className="p-5 text-center text-sm text-muted-foreground">
+        © {currentYear} {APP_NAME}. All rights reserved.
+      </div>
     </footer>
   );
 };
