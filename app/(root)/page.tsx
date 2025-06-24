3b31b42b1f450476c32b1bae954521b4155686b1
Xuwei <qinxuwei15@gmail.com>
2025-06-24 22:47:00 +0200

    project finish!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,8 @@ import {
 import ProductList from '@/components/shared/product/product-list';
 import ProductCarousel from '@/components/shared/product/product-carousel';
 import ViewAllProductsButton from '@/components/view-all-products-button';
+import IconBoxes from '@/components/icon-boxes';
+import DealCountdown from '@/components/deal-countdown';
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
   const featuredProducts = await getFeaturedProducts();
@@ -15,6 +17,8 @@ const Homepage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 };
